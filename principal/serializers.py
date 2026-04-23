72c14d66a9c407d67b56cfe32fdd526135bae946
Luma Gabriela <lumagabriela3331@gmail.com>
2026-04-23 16:59:22 -0300

    add sale_items when adding a sale

--- principal/serializers.py
+++ principal/serializers.py
@@ -1,9 +1,10 @@
 from principal.models import (State, City, Zone, District, BranchOffice, MaritalStatus, Customer,
-							  Department,
-							  Employee, Sale, MeansPayment, SalePayment, ProductGroup, Supplier,
-							  Product, SaleItem)
+                              Department,
+                              Employee, Sale, MeansPayment, SalePayment, ProductGroup, Supplier,
+                              Product, SaleItem)
 
 from rest_framework import serializers
+from django.db import transaction
 
 
 class StateSerializer(serializers.ModelSerializer):
@@ -51,7 +52,8 @@ class MaritalStatusSerializer(serializers.ModelSerializer):
 class CustomerSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Customer
-		fields = ['id', 'marital_status', 'district', 'name', 'income', 'gender', 'is_active', 'created_at', 'modified_at']
+		fields = ['id', 'marital_status', 'district', 'name', 'income', 'gender', 'is_active',
+		          'created_at', 'modified_at']
 		read_only_fields = ['id', 'created_at', 'modified_at']
 
 
@@ -65,15 +67,41 @@ class DepartmentSerializer(serializers.ModelSerializer):
 class EmployeeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Employee
-		fields = ['id', 'district', 'department', 'marital_status', 'name', 'salary', 'admission', 'birth', 'is_active', 'created_at', 'modified_at']
+		fields = ['id', 'district', 'department', 'marital_status', 'name', 'salary', 'admission',
+		          'birth', 'is_active', 'created_at', 'modified_at']
 		read_only_fields = ['id', 'created_at', 'modified_at']
 
+class SaleItemReadSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = SaleItem
+		fields = ['product', 'quantity']
+		read_only_fields = ['id', 'created_at', 'modified_at']
 
 class SaleSerializer(serializers.ModelSerializer):
+	items_data = serializers.JSONField(write_only=True)
+	sale_items = SaleItemReadSerializer(many=True, read_only=True)
+
 	class Meta:
 		model = Sale
-		fields = '__all__'
+		fields = ['branch_office', 'employee', 'customer', 'sold_at', 'items_data', 'sale_items']
 		read_only_fields = ['id', 'created_at', 'modified_at']
+
+	def create(self, validated_data):
+		items_data = validated_data.pop('items_data')
+		print(items_data)
+
+		with transaction.atomic():
+			sale = Sale.objects.create(**validated_data)
+
+			for item in items_data:
+				SaleItem.objects.create(
+					sale=sale,
+					product_id=item['product_id'],
+					quantity=item['quantity'],
+
+				)
+			sale.refresh_from_db()
+			return sale
 
 
 class MeansPaymentSerializer(serializers.ModelSerializer):
@@ -93,7 +121,8 @@ class SalePaymentSerializer(serializers.ModelSerializer):
 class ProductGroupSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ProductGroup
-		fields = ['id', 'name', 'commission_percentage', 'gain_percentage', 'is_active', 'created_at', 'modified_at']
+		fields = ['id', 'name', 'commission_percentage', 'gain_percentage', 'is_active',
+		          'created_at', 'modified_at']
 		read_only_fields = ['id', 'created_at', 'modified_at']
 
 
@@ -107,7 +136,8 @@ class SupplierSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Product
-		fields = ['id', 'product_group', 'supplier', 'name', 'cost_price', 'sale_price', 'is_active', 'created_at', 'modified_at']
+		fields = ['id', 'product_group', 'supplier', 'name', 'cost_price', 'sale_price',
+		          'is_active', 'created_at', 'modified_at']
 		read_only_fields = ['id', 'created_at', 'modified_at']
 
 
